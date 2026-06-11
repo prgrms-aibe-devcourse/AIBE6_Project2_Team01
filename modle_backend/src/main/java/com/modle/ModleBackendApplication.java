892534bacdbeb35488ad82b8231163ec3b4f3a35
락현 <krh0323@pukyong.ac.kr>
2026-06-11 11:13:44 +0900

    [Chore] 프로젝트 초기 세팅
    [#1] 프로젝트 세팅

---
+++ modle_backend/src/main/java/com/modle/ModleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.modle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModleBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModleBackendApplication.class, args);
+    }
+
+}
---
+++ modle_backend/src/test/java/com/modle/ModleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModleBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
