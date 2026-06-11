dc029745c23571d8e91824ff37053c08b2c3c010
wooki0123 <wookimini22@gmail.com>
2026-06-11 16:36:26 +0900

    [Feat] [Backend] 공고 CRUDD API ê임시 구현

---
+++ modle_backend/src/main/java/com/modle/domain/jobposting/dto/request/JobPostingCreateRequest.java
@@ -0,0 +1,15 @@
+package com.modle.domain.jobposting.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record JobPostingCreateRequest(
+        @NotBlank(message = "제목은 필수입니다.")
+        String title,
+
+        @NotBlank(message = "내용은 필수입니다.")
+        String content,
+
+        @NotBlank(message = "카테고리는 필수입니다.")
+        String category
+) {
+}
