ae6894b58f7ae380f86b9c70eb74d86ab752f52c
Rakhyunn <krh0323@pukyong.ac.kr>
2026-06-13 01:11:24 +0900

    [Chore] develop 파일 통일

--- modle_backend/src/main/java/com/modle/domain/jobposting/exception/JobPostingNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.modle.domain.jobposting.exception;
-
-public class JobPostingNotFoundException extends RuntimeException {
-
-    public JobPostingNotFoundException(Long jobPostingId) {
-        super("공고를 찾을 수 없습니다. id=" + jobPostingId);
-    }
-}
