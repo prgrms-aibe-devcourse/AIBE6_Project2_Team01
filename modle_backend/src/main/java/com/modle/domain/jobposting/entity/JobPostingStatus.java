d40c1311e66e81989a5557fc5a11c25e48a96b8c
wooki0123 <wookimini22@gmail.com>
2026-06-15 16:23:11 +0900

    [Feat] 공고 엔티티 상태 변경 메서드 추가

--- modle_backend/src/main/java/com/modle/domain/jobposting/entity/JobPostingStatus.java
+++ modle_backend/src/main/java/com/modle/domain/jobposting/entity/JobPostingStatus.java
@@ -1,10 +1,20 @@
 package com.modle.domain.jobposting.entity;
 
+import java.util.Set;
+
 public enum JobPostingStatus {
     RECRUITING, // 모집 중
     SHOOTING,   // 촬영 중
     COMPLETED,  // 완료
     CANCELLED,  // 취소
     ON_HOLD,    // 보류
-    CLOSED      // 마감
+    CLOSED;     // 마감
+
+    public boolean canTransitionTo(JobPostingStatus next) {
+        return switch (this) {
+            case RECRUITING -> Set.of(SHOOTING, CANCELLED, ON_HOLD, CLOSED).contains(next);
+            case SHOOTING   -> Set.of(COMPLETED, CANCELLED, ON_HOLD).contains(next);
+            default         -> false;
+        };
+    }
 }
