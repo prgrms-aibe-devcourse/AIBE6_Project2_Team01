1e7ac5afc331d995dd9b2f3a8331390e0d766760
wooki0123 <wookimini22@gmail.com>
2026-06-16 17:07:27 +0900

    [Feat] Application Entity, Repository,응답 DTO 생성

---
+++ modle_backend/src/main/java/com/modle/domain/application/entity/type/ApplicationStatus.java
@@ -0,0 +1,10 @@
+package com.modle.domain.application.entity.type;
+
+public enum ApplicationStatus {
+    APPLIED,        // 지원 완료
+    CONTACTED,      // 컨택 완료
+    CONTRACT_SENT,  // 계약서 발송
+    SHOOTING,       // 촬영 진행
+    COMPLETED,      // 완료
+    CANCELLED       // 취소
+}
