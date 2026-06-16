30c59c154053eefff1340a943ea2dceb75274dd7
SHINJAEHEE-DEV <asjh011969@gmail.com>
2026-06-16 12:33:45 +0900

    [Feat] Client 마이페이지 백엔드지역 수정 메서드추가

--- modle_backend/src/main/java/com/modle/domain/profile/service/ClientService.java
+++ modle_backend/src/main/java/com/modle/domain/profile/service/ClientService.java
@@ -32,6 +32,11 @@ public class ClientService {
         clientRepository.delete(client);
     }
 
-    public void update(Client client, @NotBlank @Size(min = 2, max = 100) String componyName, ClientType clientType, String introduction,String region,String newImageUrl) {
+    public void update(Client client, @NotBlank @Size(min = 2, max = 100) String companyName, ClientType clientType, String introduction, String region, String newImageUrl) {
+        client.update(companyName, client.getCompanyNumber(), clientType, introduction, newImageUrl);
+        if (region != null) {
+            client.getUser().updateRegion(region);
+        }
+        clientRepository.save(client);
     }
 }
