c1bff915690c3d639d09e0166a90db42b88ea6a5
Rakhyunn <krh0323@pukyong.ac.kr>
2026-06-16 11:41:43 +0900

    [Feat] 신고 엔티티 구현, 에러코드 및 메일 서비스 함수 구현

---
+++ modle_backend/src/main/java/com/modle/domain/review/entity/type/ReportTargetType.java
@@ -0,0 +1,8 @@
+package com.modle.domain.review.entity.type;
+
+public enum ReportTargetType {
+    JOB_POSTING,
+    PROFILE,
+    MESSAGE
+    // 추후: CONTRACT, NO_SHOW
+}
--- modle_backend/src/main/java/com/modle/domain/user/entity/User.java
+++ modle_backend/src/main/java/com/modle/domain/user/entity/User.java
@@ -94,4 +94,8 @@ public class User extends BaseEntity {
     public void changePassword(String encodedPassword) {
         this.password = encodedPassword;
     }
+
+    public void increaseWarningCount() {
+        this.warningCount++;
+    }
 }
