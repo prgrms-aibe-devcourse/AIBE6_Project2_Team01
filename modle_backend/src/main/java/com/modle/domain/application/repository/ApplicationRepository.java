c6f2c62e6fe844f92a27f31ee39ccf18b311b8d0
wooki0123 <wookimini22@gmail.com>
2026-06-17 10:01:43 +0900

    [Feat] ApplicationRepository 취소용 쿼리 메서드 추가

--- modle_backend/src/main/java/com/modle/domain/application/repository/ApplicationRepository.java
+++ modle_backend/src/main/java/com/modle/domain/application/repository/ApplicationRepository.java
@@ -4,6 +4,8 @@ import com.modle.domain.application.entity.Application;
 import com.modle.domain.application.entity.type.ApplicationStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ApplicationRepository extends JpaRepository<Application, Long> {
 
     // MATCH-001: 취소된 지원을 제외하고 동일 공고·동일 모델의 지원 여부 확인 (중복 지원 검증)
@@ -12,4 +14,7 @@ public interface ApplicationRepository extends JpaRepository<Application, Long> 
             Long modelId,
             ApplicationStatus status
     );
+
+    // MATCH-002: 취소 시 본인 지원 여부 확인
+    Optional<Application> findByIdAndModelId(Long id, Long modelId);
 }
