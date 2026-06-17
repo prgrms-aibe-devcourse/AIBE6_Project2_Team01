b4629a635a3615cba6e8a61ba1f77571b2693192
Rakhyunn <krh0323@pukyong.ac.kr>
2026-06-17 15:49:04 +0900

    [Feat] 지원 관리 레포지토리 함수 구현

--- modle_backend/src/main/java/com/modle/domain/application/repository/ApplicationRepository.java
+++ modle_backend/src/main/java/com/modle/domain/application/repository/ApplicationRepository.java
@@ -4,6 +4,7 @@ import com.modle.domain.application.entity.Application;
 import com.modle.domain.application.entity.type.ApplicationStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ApplicationRepository extends JpaRepository<Application, Long> {
@@ -17,4 +18,17 @@ public interface ApplicationRepository extends JpaRepository<Application, Long> 
 
     // MATCH-002: 취소 시 본인 지원 여부 확인
     Optional<Application> findByIdAndModelId(Long id, Long modelId);
+
+    // MATCH-004: 특정 공고의 지원자 목록
+    List<Application> findByJobPostingIdAndStatusNotOrderByCreatedDateDesc(
+            Long jobPostingId, ApplicationStatus status);
+
+    // MATCH-005: 모델의 지원 목록
+    List<Application> findByModelIdAndStatusNotOrderByCreatedDateDesc(
+            Long modelId, ApplicationStatus status);
+
+    // MATCH-006: 공고별 지원자 수 집계
+    long countByJobPostingIdAndStatusNot(Long jobPostingId, ApplicationStatus status);
+    long countByJobPostingIdAndStatus(Long jobPostingId, ApplicationStatus status);
+    long countByJobPostingIdAndStatusIn(Long jobPostingId, List<ApplicationStatus> statuses);
 }
