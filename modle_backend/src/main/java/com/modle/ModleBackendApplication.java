1ace6356647aae85641ff92fa68f48404987d177
Rakhyunn <krh0323@pukyong.ac.kr>
2026-06-18 14:38:49 +0900

    [Refactor] 이메일 비동기 처리 리팩토링

--- modle_backend/src/main/java/com/modle/ModleBackendApplication.java
+++ modle_backend/src/main/java/com/modle/ModleBackendApplication.java
@@ -3,11 +3,13 @@ package com.modle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing // 생성일, 수정일 자동화
 @SpringBootApplication
 @EnableScheduling
+@EnableAsync
 public class ModleBackendApplication {
 
     public static void main(String[] args) {
